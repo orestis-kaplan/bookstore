6036562e7f093c82d0f99cd1c25e53359c11c1dd
userman95 <orestismail71@gmail.com>
2019-10-01 17:56:33 +0300

    First milestone

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    'no-shadow': 0,
+    'import/no-extraneous-dependencies': 0,
+    'camelcase': 0,
+    'import/no-unresolved': 0
+  },
+};
