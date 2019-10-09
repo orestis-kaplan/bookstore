2ff4c10da4ad2c32c47839ac0523d38fe25c999c
userman95 <orestismail71@gmail.com>
2019-10-09 16:59:45 +0300

    fix the createbook action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,6 @@
 export const createBook = (book) => {
-  const newBook = book;
-  if (!newBook.id) newBook.id = Math.floor((Math.random() * 100) + 1);
-  return { type: 'CREATE_BOOK', newBook };
+  if (!book.id) book.id = Math.floor((Math.random() * 100) + 1);
+  return { type: 'CREATE_BOOK', book };
 };
 
 export const removeBook = (id) => ({ type: 'REMOVE_BOOK', id });
